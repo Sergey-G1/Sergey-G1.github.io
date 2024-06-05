6fb269ac699b84b91193b75000666cd696232474
Sergey-G1 <68800603+Sergey-G1@users.noreply.github.com>
2024-06-05 12:58:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCU__tNtteuEsmw0_uTkHwoSbSQ9snQM40",
-    authDomain: "tirabota-2dd58.firebaseapp.com",
-    projectId: "tirabota-2dd58",
-    storageBucket: "tirabota-2dd58.appspot.com",
-    messagingSenderId: "531431614879",
-    appId: "1:531431614879:web:bacf36bc88d7372d14b5aa"
+    "apiKey": "AIzaSyCU__tNtteuEsmw0_uTkHwoSbSQ9snQM40",
+    "authDomain": "tirabota-2dd58.firebaseapp.com",
+    "projectId": "tirabota-2dd58",
+    "storageBucket": "tirabota-2dd58.appspot.com",
+    "messagingSenderId": "531431614879",
+    "appId": "1:531431614879:web:bacf36bc88d7372d14b5aa"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
